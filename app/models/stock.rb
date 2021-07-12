897f879a0de30933ee62bc5402c0a481f4e40145
mariavaghani <maria.vaghani@gmail.com>
2021-07-11 22:06:58 -0400

    add new_lookup method with secure credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: 'Tpk_8c996c009d9a46cc8aa4e3721cd5b522',
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
     client.price(ticker_symbol)
   end
