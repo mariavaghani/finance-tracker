0ad92c09238fcbbbba3a38630fe80260e29eb302
mariavaghani <maria.vaghani@gmail.com>
2021-07-13 02:06:37 -0400

    add button to add to portfolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,14 @@
 class UserStocksController < ApplicationController
+  def create
+    # if params[:ticker] in current_user.stocks
+    stock = Stock.check_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
+    end
+      @user_stock = UserStock.create(user: current_user, stock: stock)
+      flash.now[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+      redirect_to my_portfolio_path
+  
+  end
 end
