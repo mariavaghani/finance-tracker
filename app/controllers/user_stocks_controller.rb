9385535390c38c3639eefbabac43eaaaffd9aa6f
mariavaghani <maria.vaghani@gmail.com>
2021-07-13 13:30:23 -0400

    add remove tracking stocks action

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,6 +9,14 @@ class UserStocksController < ApplicationController
       @user_stock = UserStock.create(user: current_user, stock: stock)
       flash.now[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
       redirect_to my_portfolio_path
-  
+
+  end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(stock_id: stock.id, user_id: current_user.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from your portfolio"
+    redirect_to my_portfolio_path
   end
 end
