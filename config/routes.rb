cbe7eda188646fb175f4103ef73e95eef28ac73f
mariavaghani <maria.vaghani@gmail.com>
2021-07-14 04:47:05 -0400

    add user profile

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "my_portfolio", to: "users#my_portfolio"
   get "search_stock", to: "stocks#search"
   get "search_friend", to: "friends#search"
+  resources :users, only: [:show]
 end
